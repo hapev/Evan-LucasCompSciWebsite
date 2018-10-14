309e82b268fe619ba1f62572726a2236237e2415
hapev <evan.coats.21@bishops.com>
2018-10-13 17:49:48 -0700

    created working login page

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,45 @@
-var name = "Lucas";
-var welcomeResult = "Welcome "+name+"!";
-document.getElementById("welcome").innerHTML=welcomeResult;
+
+if (sessionStorage.getItem("firstName")!=""&&sessionStorage.getItem("firstName")!=null){
+document.getElementById("welcome").innerHTML="Welcome " + sessionStorage.getItem("firstName")+ "!";
+}
+
+function submitId(){
+
+var firstNameIn = document.getElementById("FirstNameInput").value
+var lastNameIn = document.getElementById("LastNameInput").value;
+var loggedInBoolean = "yes";
+sessionStorage.setItem("firstName",firstNameIn);
+sessionStorage.setItem("lastName",lastNameIn);
+sessionStorage.setItem("loggedIn",loggedInBoolean);
+window.location.replace('../index.html');
+
+}
+
+
+
+
+function signedIn(){
+    if (sessionStorage.getItem("loggedIn")==="yes"){
+        return true;
+    }
+    return false;
+}
+
+if(signedIn()){
+    
+                document.getElementById("loginTab").innerHTML="Logout";
+}
+
+function logout(){
+    
+    if (signedIn()){
+        
+        var nal = "";
+        
+        sessionStorage.setItem("firstName",nal);
+        sessionStorage.setItem("lastName",nal);
+        sessionStorage.setItem("loggedIn",nal);
+
+    }
+    
+}
