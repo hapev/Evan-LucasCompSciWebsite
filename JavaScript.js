ffb150de24fdbbb10d7f91d07394a3169d8bf27a
hapev <evan.coats.21@bishops.com>
2018-10-23 10:57:11 -0700

    added wow.js

--- JavaScript.js
+++ JavaScript.js
@@ -5,25 +5,25 @@ if (sessionStorage.getItem("loggedIn")==="yes"){
 }
 
 if (sessionStorage.getItem("firstName")!=""&&sessionStorage.getItem("firstName")!=null){
-document.getElementById("welcome").innerHTML="Welcome " + sessionStorage.getItem("firstName")+ "!";
+    document.getElementById("welcome").innerHTML="Welcome " + sessionStorage.getItem("firstName")+ "!";
 }
 
 function submitId(){
 
-var firstNameIn = document.getElementById("FirstNameInput").value;
-var lastNameIn = document.getElementById("LastNameInput").value;
-var loggedInBoolean = "yes";
-sessionStorage.setItem("firstName",firstNameIn);
-sessionStorage.setItem("lastName",lastNameIn);
-sessionStorage.setItem("loggedIn",loggedInBoolean);
-window.location.replace('../index.html');
+    var firstNameIn = document.getElementById("FirstNameInput").value;
+    var lastNameIn = document.getElementById("LastNameInput").value;
+    var loggedInBoolean = "yes";
+    sessionStorage.setItem("firstName",firstNameIn);
+    sessionStorage.setItem("lastName",lastNameIn);
+    sessionStorage.setItem("loggedIn",loggedInBoolean);
+    window.location.replace('../index.html');
 
 }
 
 
 function signedIn(){
     if (sessionStorage.getItem("loggedIn")==="yes"){
-               console.log("Called");
+        console.log("Called");
 
         return true;
     }
@@ -38,14 +38,15 @@ function signedIn(){
 
 
 function logout(){
-    
+
     if (signedIn()){
-        
+
         var nal = "";
-        
+
         sessionStorage.setItem("firstName",nal);
         sessionStorage.setItem("lastName",nal);
         sessionStorage.setItem("loggedIn",nal);
+
 
     }
 }
